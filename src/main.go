0a2db3c9699b63a5bbcf327666df6f87b26601d6
osvaldsoza <osvald.soza@gmail.com>
2022-02-09 22:22:49 -0300

    incluindo dependencia

--- src/main.go
+++ src/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/badoux/checkmail"
 	"osf/util"
 )
 
 func main() {
 	fmt.Println("Hello World")
 	util.ShowMessage()
+	erro := checkmail.ValidateFormat("osvald.soza@gmail.com")
+	fmt.Println(erro)
 }
